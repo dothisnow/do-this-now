778a5914600cd3907e48121a00fa15a635c1f6f9
Max Lascombe <mtl2154@columbia.edu>
2022-08-19 09:47:25 -0400

    Added date as argument for top and done api functions. This should resolve have tasks from tomorrow showing up in the evening.

--- src/hooks/useQueryTasksTop.js
+++ src/hooks/useQueryTasksTop.js
@@ -5,7 +5,13 @@ export const useQueryTasksTop = () =>
     useQuery(
         ['tasks-top'],
         () => {
-            return API.get('tasks', '/tasks/top', {})
+            return API.get('tasks', '/tasks/top', {
+                queryStringParameters: {
+                    date: `${new Date().getFullYear()}-${
+                        new Date().getMonth() + 1
+                    }-${new Date().getDate()}`,
+                },
+            })
         },
         {
             refetchInterval: 1000 * 2,

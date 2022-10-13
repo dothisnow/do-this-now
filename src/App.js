c03cf9968b84945318eaf051e655f632190c61da
Max Lascombe <mtl2154@columbia.edu>
2022-10-13 13:55:23 -0400

    Removed pointer events on computer.

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import UpdateTask from './updatetask'
 
 const App = () => {
     return (
-        <div className='w-100vw h-100vh bg-gray-900'>
+        <div className="md:pointer-events-none w-100vw h-100vh bg-gray-900">
             <Router>
                 <Routes>
-                    <Route path='/'>
+                    <Route path="/">
                         <Route index element={<Home />} />
-                        <Route path='login' element={<Login />} />
-                        <Route path='new-task' element={<NewTask />} />
-                        <Route path='tasks' element={<Tasks />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="new-task" element={<NewTask />} />
+                        <Route path="tasks" element={<Tasks />} />
                         <Route
-                            path='update-task/:id'
+                            path="update-task/:id"
                             element={<UpdateTask />}
                         />
                     </Route>
@@ -27,8 +27,8 @@ const App = () => {
             </Router>
             <button
                 onClick={loginManager.signOut}
-                type='button'
-                className='fixed right-5 bottom-5 px-2.5 py-1.5 border border-gray-700 text-xs text-white font-medium rounded bg-gray-800 hover:bg-gray-700 hover:border-gray-600'>
+                type="button"
+                className="fixed right-5 bottom-5 px-2.5 py-1.5 border border-gray-700 text-xs text-white font-medium rounded bg-gray-800 hover:bg-gray-700 hover:border-gray-600">
                 Log out
             </button>
         </div>

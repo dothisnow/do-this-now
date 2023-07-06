34ab93c727eafdd6249fc512cda795fc587333c8
Max Lascombe <mtl2154@columbia.edu>
2023-07-05 22:54:56 -0400

    Added zod, checked what is returned by progress api with zod, and simplified progress component.

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -2,38 +2,46 @@ import { minutesToHours } from '../helpers/time'
 import { useQueryProgressToday } from '../hooks/useQueryProgressToday'
 
 const Progress = () => {
-  const { data: progress, isLoading: isLoadingProgress } =
-    useQueryProgressToday()
+  const progress = useQueryProgressToday()
 
-  const tasksDoneToday =
-    (progress?.done ?? 0) + (progress?.doneBeforeToday ?? 0)
+  if (progress.data === undefined) return <></>
 
-  if (isLoadingProgress) return <></>
+  const { done, streak, streakIsActive, todo } = progress.data
+
   return (
     <div className='mb-2 flex flex-col items-center'>
+      {streak > 0 && (
+        <div className='text-xs font-bold leading-3 text-gray-300'>
+          Streak:{' '}
+          {streakIsActive ? (
+            (streak > 0 && streak <= 3 ? '🔥'.repeat(streak) : streak) +
+            (streak > 3 ? '🔥' : '')
+          ) : (
+            <span className='grayscale'>🔥</span>
+          )}
+        </div>
+      )}
       <div className='text-xs font-bold leading-3 text-gray-300'>
-        {tasksDoneToday - (progress?.todo ?? 0) > 0
-          ? `+ ${minutesToHours(tasksDoneToday - (progress?.todo ?? 10))}`
-          : `${minutesToHours(tasksDoneToday)} / ${minutesToHours(
-              progress?.todo ?? 10
-            )}`}
+        {done - todo > 0
+          ? `+ ${minutesToHours(done - todo)}`
+          : `${minutesToHours(done)} / ${minutesToHours(todo)}`}
         {/** - end: ${minutesToHours(
                       //   (currentTime.getHours() * 60 +
                       //     currentTime.getMinutes() +
-                      //     (progress?.todo ?? 10) -
-                      //     tasksDoneToday) %
+                      //     todo  -
+                      //     done) %
                       //     (12 * 60)
                       // )}`}**/}
       </div>
       <div
-        style={{ width: Math.round((144 * (progress?.todo ?? 600)) / 600) }}
+        style={{
+          width: Math.round((144 * todo) / 600),
+        }}
         className='mt-0.5 h-2 overflow-hidden rounded-full border border-gray-700 bg-gray-800'>
         <div
           className='h-full rounded-full bg-white'
           style={{
-            width:
-              Math.min((tasksDoneToday / (progress?.todo ?? 10)) * 100, 100) +
-              '%',
+            width: Math.min((done / todo) * 100, 100) + '%',
           }}
         />
       </div>
--- src/hooks/useQueryProgressToday.ts
+++ src/hooks/useQueryProgressToday.ts
@@ -1,16 +1,26 @@
 import API from '@aws-amplify/api'
 import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
 
 import { dateString } from '../helpers/dates'
+
+export const progressTodaySchema = z.object({
+  done: z.number(),
+  todo: z.number(),
+  streak: z.number(),
+  streakIsActive: z.boolean(),
+})
 
 export const useQueryProgressToday = () => {
   const date = dateString(new Date())
   return useQuery(
     ['progress-today', date],
     async () => {
-      return API.get('tasks', '/tasks/progresstoday', {
-        queryStringParameters: { date },
-      }).catch(console.error)
+      return progressTodaySchema.parse(
+        await API.get('tasks', '/tasks/progresstoday', {
+          queryStringParameters: { date },
+        })
+      )
     },
     {
       refetchInterval: 1000 * 2,

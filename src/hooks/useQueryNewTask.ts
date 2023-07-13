2d7abcbef141e4dd7530103826101cfefad266e1
Max Lascombe <mtl2154@columbia.edu>
2023-07-12 20:59:59 -0400

    Added xod validation to new task mutation.

--- src/hooks/useQueryNewTask.ts
+++ src/hooks/useQueryNewTask.ts
@@ -1,14 +1,15 @@
 import API from '@aws-amplify/api'
 import { useMutation } from '@tanstack/react-query'
+import { z } from 'zod'
 
 import { Task } from '../types/task'
 
 export const useQueryNewTask = () => {
   return useMutation(
     async (task: Task) => {
-      return API.post('tasks', '/tasks/new', { body: task }).catch(
-        console.error
-      )
+      return z
+        .literal('Success')
+        .parse(await API.post('tasks', '/tasks/new', { body: task }))
     },
     { onSuccess: () => window.history.back() }
   )

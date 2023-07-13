9b8e2c60bec98c7ed877da6fc88d13c8881268d1
Max Lascombe <mtl2154@columbia.edu>
2023-07-12 21:04:18 -0400

    Added zod validation to snooze mutation query.

--- src/hooks/useQuerySnoozeTask.ts
+++ src/hooks/useQuerySnoozeTask.ts
@@ -1,12 +1,15 @@
 import API from '@aws-amplify/api'
 import { useMutation } from '@tanstack/react-query'
+import { z } from 'zod'
 
 export const useQuerySnoozeTask = () => {
   return useMutation(async ({ title }: { title: string }) => {
-    return API.post('tasks', '/tasks/snooze', {
-      body: {
-        title,
-      },
-    }).catch(console.error)
+    return z.object({}).parse(
+      await API.post('tasks', '/tasks/snooze', {
+        body: {
+          title,
+        },
+      })
+    )
   })
 }

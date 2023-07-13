21e445061c41ba38fe63636f073ac1636b11859e
Max Lascombe <mtl2154@columbia.edu>
2023-07-12 21:17:40 -0400

    Added zod validation to done mutation query.

--- src/hooks/useQueryTaskDone.ts
+++ src/hooks/useQueryTaskDone.ts
@@ -1,17 +1,18 @@
 import API from '@aws-amplify/api'
 import { useMutation } from '@tanstack/react-query'
+import { z } from 'zod'
 
 export const useQueryTaskDone = () => {
-  return useMutation(
-    async task => {
-      return API.post('tasks', '/tasks/done', {
+  return useMutation(async task => {
+    return z.object({}).parse(
+      await API.post('tasks', '/tasks/done', {
         body: {
           task,
           date: `${new Date().getFullYear()}-${
             new Date().getMonth() + 1
           }-${new Date().getDate()}`,
         },
-      }).catch(console.error)
-    }
-  )
+      })
+    )
+  })
 }

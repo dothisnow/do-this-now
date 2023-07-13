6fb905f6356474aa24de38720d84efe0cd1a100d
Max Lascombe <mtl2154@columbia.edu>
2023-07-12 21:14:44 -0400

    Added zod validation to delete mutation query.

--- src/hooks/useQueryTaskDelete.ts
+++ src/hooks/useQueryTaskDelete.ts
@@ -1,12 +1,11 @@
 import API from '@aws-amplify/api'
 import { useMutation } from '@tanstack/react-query'
+import { z } from 'zod'
 
 export const useQueryTaskDelete = () => {
-  return useMutation(
-    async task => {
-      return API.post('tasks', '/tasks/delete', { body: task }).catch(
-        console.error
-      )
-    }
-  )
+  return useMutation(async task => {
+    return z
+      .object({})
+      .parse(await API.post('tasks', '/tasks/delete', { body: task }))
+  })
 }

c6b07779c5fb31b36b0084410c8180de60ee172b
Max Lascombe <mtl2154@columbia.edu>
2023-07-27 19:00:14 -0400

    Finished separating the switch component to centralize the styling.

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -1,21 +1,28 @@
-import { ComponentProps } from 'react'
-import { Switch as OldSwitch } from '@headlessui/react'
+import { Switch as UnwrappedSwitch } from '@headlessui/react'
 
-export const Switch = ({ checked, onChange, className }: { checked: boolean; onChange: (checked: boolean) => void; className?: string }) => (
-          <OldSwitch
-            checked={checked}
-            onChange={onChange}
-            className={
-              (strictDeadline ? 'bg-blue-600' : 'bg-gray-200') +
-              ' relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent outline-none ring-white ring-offset-0 ring-offset-black transition-colors duration-200 ease-in-out focus:z-10 focus:outline-none focus:ring-2 focus:ring focus:ring-blue-500 focus:ring-offset-2'
-            }>
-            <span className='sr-only'>Use setting</span>
-            <span
-              aria-hidden='true'
-              className={
-                (strictDeadline ? 'translate-x-5' : 'translate-x-0') +
-                ' pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
-              }
-            />
-                  )
-
+export const Switch = ({
+  checked,
+  onChange,
+  className,
+}: {
+  checked: boolean
+  onChange: (checked: boolean) => void
+  className?: string
+}) => (
+  <UnwrappedSwitch
+    checked={checked}
+    onChange={onChange}
+    className={
+      (checked ? 'bg-blue-600' : 'bg-gray-200') +
+      ' relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent outline-none ring-white ring-offset-0 ring-offset-black transition-colors duration-200 ease-in-out focus:z-10 focus:outline-none focus:ring-2 focus:ring focus:ring-blue-500 focus:ring-offset-2 ' +
+      className
+    }>
+    <span className='sr-only'>Use setting</span>
+    <span
+      aria-hidden='true'
+      className={
+        (checked ? 'translate-x-5' : 'translate-x-0') +
+        ' pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+      }></span>
+  </UnwrappedSwitch>
+)
--- src/components/taskform.tsx
+++ src/components/taskform.tsx
@@ -1,4 +1,3 @@
-import { Switch } from '@headlessui/react'
 import {
   CheckCircleIcon,
   PlusCircleIcon,
@@ -8,6 +7,7 @@ import { format } from 'date-fns'
 import { ComponentProps, Dispatch, SetStateAction, useState } from 'react'
 
 import { RepeatOption, RepeatUnit, TaskInput } from '../types/task'
+import { Switch } from './switch'
 
 const TaskForm = ({
   titleState: [title, setTitle],
@@ -166,18 +166,9 @@ const TaskForm = ({
             checked={strictDeadline}
             onChange={setStrictDeadline}
             className={
-              (strictDeadline ? 'bg-blue-600' : 'bg-gray-200') +
-              ' relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent outline-none ring-white ring-offset-0 ring-offset-black transition-colors duration-200 ease-in-out focus:z-10 focus:outline-none focus:ring-2 focus:ring focus:ring-blue-500 focus:ring-offset-2'
-            }>
-            <span className='sr-only'>Use setting</span>
-            <span
-              aria-hidden='true'
-              className={
-                (strictDeadline ? 'translate-x-5' : 'translate-x-0') +
-                ' pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
-              }
-            />
-          </Switch>
+              'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent outline-none ring-white ring-offset-0 ring-offset-black transition-colors duration-200 ease-in-out focus:z-10 focus:outline-none focus:ring-2 focus:ring focus:ring-blue-500 focus:ring-offset-2'
+            }
+          />
         </div>
       </div>
 
@@ -298,18 +289,9 @@ const TaskForm = ({
                 setHasSubtasks(e)
               }}
               className={
-                (hasSubtasks ? 'bg-blue-600' : 'bg-gray-200') +
-                ' relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
-              }>
-              <span className='sr-only'>Use setting</span>
-              <span
-                aria-hidden='true'
-                className={
-                  (hasSubtasks ? 'translate-x-5' : 'translate-x-0') +
-                  ' pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
-                }
-              />
-            </Switch>
+                'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+              }
+            />
           </div>
           {hasSubtasks && (
             <>

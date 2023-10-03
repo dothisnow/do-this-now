a399b4e8998783d7b93c04724f4d2f2de8c60b3f
Max Lascombe <mtl2154@columbia.edu>
2023-10-03 12:42:22 +0200

    Started working on redesigning the progress component.

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -1,4 +1,8 @@
-import { ExclamationCircleIcon, FireIcon } from '@heroicons/react/20/solid'
+import {
+  ExclamationCircleIcon,
+  FireIcon,
+  HeartIcon,
+} from '@heroicons/react/20/solid'
 import { minutesToHours } from '../helpers/time'
 import { useQueryProgressToday } from '../hooks/useQueryProgressToday'
 
@@ -7,43 +11,55 @@ const Progress = () => {
 
   if (progress.data === undefined) return <></>
 
-  const { done, streak, streakIsActive, todo } = progress.data
+  const { done, lives, streak, streakIsActive, todo } = {
+    done: 500,
+    lives: 100,
+    streak: 5,
+    streakIsActive: true,
+    todo: 560,
+  } //progress.data
 
   return (
-    <div className='mb-2 flex flex-col items-center'>
-      {streak > 0 && (
-        <div className='flex text-xs font-bold leading-6 text-gray-300'>
-          Streak: {streak}
+    <div className='flex justify-center'>
+      <div className='mb-2 flex flex-col items-center rounded border border-gray-700 bg-gray-800 p-2 drop-shadow-sm'>
+        <div className='flex text-sm font-bold text-gray-300'>
           {streakIsActive ? (
-            <FireIcon className='mt-1 block h-4 w-4' />
+            <FireIcon className='block h-5 w-5' />
           ) : (
-            <ExclamationCircleIcon className='mt-1 block h-4 w-4' />
-          )}
+            <ExclamationCircleIcon className='block h-3 w-3' />
+          )}{' '}
+          {streak}
         </div>
-      )}
-      <div className='text-xs font-bold leading-6 text-gray-300'>
-        {done - todo > 0
-          ? `+ ${minutesToHours(done - todo)}`
-          : `${minutesToHours(done)} / ${minutesToHours(todo)}`}
-        {/** - end: ${minutesToHours(
+
+        <div className='flex text-xs font-bold text-gray-300'>
+          <HeartIcon className='block h-4 w-4' /> {minutesToHours(lives)} -{' '}
+          {minutesToHours(Math.min(lives, todo - done))}
+        </div>
+
+        <div className='text-xs font-bold leading-6 text-gray-300'>
+          {done - todo > 0
+            ? `+ ${minutesToHours(done - todo)}`
+            : `${minutesToHours(done)} / ${minutesToHours(todo)}`}
+          {/** - end: ${minutesToHours(
                       //   (currentTime.getHours() * 60 +
                       //     currentTime.getMinutes() +
                       //     todo  -
                       //     done) %
                       //     (12 * 60)
                       // )}`}**/}
-      </div>
-      <div
-        style={{
-          width: Math.round((144 * todo) / 600),
-        }}
-        className='mt-0.5 h-2 overflow-hidden rounded-full border border-gray-700 bg-gray-800'>
+        </div>
         <div
-          className='h-full rounded-full bg-white'
           style={{
-            width: Math.min((done / todo) * 100, 100) + '%',
+            width: Math.round((144 * todo) / 600),
           }}
-        />
+          className='mt-0.5 h-2 overflow-hidden rounded-full border border-gray-700'>
+          <div
+            className='h-full rounded-full bg-white'
+            style={{
+              width: Math.min((done / todo) * 100, 100) + '%',
+            }}
+          />
+        </div>
       </div>
     </div>
   )

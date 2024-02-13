9de8e267f763a64fb35183f0b96b873cb7e530e4
Max Lascombe <mtl2154@columbia.edu>
2024-02-13 17:42:39 +0800

    Removed blinking dot in last updated component.

--- src/components/lastupdated.tsx
+++ src/components/lastupdated.tsx
@@ -10,9 +10,6 @@ export const LastUpdated = ({
   isFetching: boolean
 }) => (
   <div className='flex items-center gap-2 text-xs text-gray-700'>
-    {isFetching && (
-      <div className='h-1 w-1 animate-ping rounded-full bg-gray-400 opacity-75' />
-    )}
-    Last updated {ago(new Date(at))}
+    {isFetching ? 'Checking for updates...' : `Updated ${ago(new Date(at))}`}
   </div>
 )

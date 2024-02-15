ebabc438567a4feeb6fc23875713961b258bf844
Max Lascombe <mtl2154@columbia.edu>
2024-02-15 23:53:08 +0800

    Update last updated component once a second.

--- src/components/lastupdated.tsx
+++ src/components/lastupdated.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react'
 import ago from 's-ago'
 
 type Timestamp = number
+
+const useNow = () => {
+  const [now, setNow] = useState(new Date())
+  useEffect(() => {
+    const interval = setInterval(() => setNow(new Date()), 1000)
+    return () => clearInterval(interval)
+  }, [])
+  return now
+}
 
 export const LastUpdated = ({
   at,
@@ -8,8 +18,11 @@ export const LastUpdated = ({
 }: {
   at: Timestamp
   isFetching: boolean
-}) => (
-  <div className='flex items-center gap-2 text-xs text-gray-700'>
-    {isFetching ? 'Checking for updates...' : `Updated ${ago(new Date(at))}`}
-  </div>
-)
+}) => {
+  useNow()
+  return (
+    <div className='flex items-center gap-2 text-xs text-gray-700'>
+      {isFetching ? 'Checking for updates...' : `Updated ${ago(new Date(at))}`}
+    </div>
+  )
+}

01993f1b4e38b5b02855a5a4414b4eae12bb32e5
Max Lascombe <mtl2154@columbia.edu>
2024-03-15 13:11:33 +0800

    Removed unused accent color from tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        accent: {
-          50: '#ebffff',
-          100: '#cdfbff',
-          200: '#a2f6ff',
-          300: '#62edfe',
-          400: '#1bd9f5',
-          500: '#00bcdb',
-          600: '#029ec2',
-          700: '#097895',
-          800: '#126178',
-          900: '#135066',
-          950: '#063446',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }

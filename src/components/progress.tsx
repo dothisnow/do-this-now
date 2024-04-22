d3e22f93e6d831e3bb9a8d11ebcbb9f42dae8be1
Max Lascombe <mtl2154@columbia.edu>
2024-04-22 10:39:35 +0800

    Added points to progress section, next need to add highscore.

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -1,4 +1,4 @@
-import { faFire, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faFire, faHeart, faStar } from '@fortawesome/free-solid-svg-icons'
 import { minutesToHours } from '../helpers/time'
 import { useQueryProgressToday } from '../hooks/useQueryProgressToday'
 import { Tag } from './tags'
@@ -13,16 +13,25 @@ const Progress = () => {
   const livesUsed = Math.min(lives, todo - done)
   const livesLeft = lives - livesUsed
 
+  const doneUsingAllLives = Math.min(done, todo - lives)
+  const doneUsingLives = Math.min(done, todo)
+  const points =
+    doneUsingAllLives +
+    (doneUsingLives - doneUsingAllLives) * 2 +
+    (done - doneUsingLives) * 3
+
   return (
     <div className='flex justify-center'>
       <div className='flex flex-col items-center gap-1 text-xs font-light'>
         <div className='flex w-full justify-center gap-5 text-white'>
+          <Tag icon={faStar} text={'' + points} />
           <Tag
             icon={faFire}
             text={'' + streak}
             color={streakIsActive ? 'text-amber-500' : 'text-white/50'}
           />
-
+        </div>
+        <div className='flex w-full justify-center gap-5 text-white'>
           {livesLeft > 0 ? (
             <Tag
               icon={faHeart}

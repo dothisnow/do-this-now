483d0e4bf925653aff491afe33f9cdd2c09ea96e
Max Lascombe <mtl2154@columbia.edu>
2024-07-05 11:23:00 +0800

    Fixed task sorting algorithm that would randomly sort tasks due on the same day.

--- shared-logic/task-sorting.ts
+++ shared-logic/task-sorting.ts
@@ -51,7 +51,8 @@ export const sortTasks = (tasks: Task[], today: Date) => {
       if (flag(a) && !flag(b)) return -1
       if (flag(b) && !flag(a)) return 1
     }
-    if ('due' in a && 'due' in b) return a.due.localeCompare(b.due)
+    if ('due' in a && 'due' in b && a.due.localeCompare(b.due) !== 0)
+      return a.due.localeCompare(b.due)
     if ('timeFrame' in a && 'timeFrame' in b) {
       if (a.timeFrame === 0) return -1
       if (b.timeFrame === 0) return 1

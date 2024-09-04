a566b513562574368909b38b0186e64f2bdac63f
Max Lascombe <mtl2154@columbia.edu>
2024-09-04 19:04:07 +0800

    Added r key action to allow to reload query.

--- src/components/lastupdated.tsx
+++ src/components/lastupdated.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import ago from 's-ago'
+import useKeyAction from '../hooks/useKeyAction'
 
 const useNow = () => {
   const [now, setNow] = useState(new Date())
@@ -17,13 +18,23 @@ export const LastUpdated = ({
   query: ReturnType<typeof useQuery>
 }) => {
   useNow()
+  useKeyAction([
+    {
+      key: 'r',
+      action: () => query.refetch(),
+      description: 'Refresh',
+    },
+  ])
   return (
     <button
       onClick={() => query.refetch()}
-      className='flex items-center gap-2 text-xs text-gray-700'>
+      className='group flex items-center gap-2 text-xs text-gray-700'>
       {query.isFetching
         ? 'Checking for updates...'
         : `Updated ${ago(new Date(query.dataUpdatedAt))}`}
+      {!query.isFetching && (
+        <span className='hidden underline group-hover:inline'>Refresh</span>
+      )}
     </button>
   )
 }

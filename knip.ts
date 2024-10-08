1b5b3affdbbfab281629caa8a7583e7c98b95659
Max Lascombe <MaxLascombe@users.noreply.github.com>
2024-10-08 23:48:38 +0800

    Knip (#61)
    * Added knip.
    
    * Copied in new ci.
    
    * Ran prettier.
    
    * Removed amplifyconfig from gitignore.

--- knip.ts
+++ knip.ts
@@ -1,8 +1,8 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  entry: ['src/index.tsx'],
-  ignore: ['amplify/**/*.{js,ts}', 'lambda-layers/**/*.js'],
+  entry: ['src/index.tsx', 'src/shared-logic/*.ts', 'src/types/task.ts'],
+  ignore: ['amplify/**/*.{js,ts}', 'lambda-layers/**/*.js', 'vite-env.d.ts'],
 }
 
 export default config

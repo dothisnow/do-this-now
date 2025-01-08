14f4574cc6d52a8bc5d6a638945a9800d5af13f8
Max Lascombe <mtl2154@columbia.edu>
2025-01-08 20:50:33 +0800

    Ran amplify push.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,118 +1,118 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    tasks: {
-      ApiId: 'string'
-      ApiName: 'string'
-      RootUrl: 'string'
+  "api": {
+    "tasks": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
     }
-  }
-  auth: {
-    maxlascombe: {
-      AppClientID: 'string'
-      AppClientIDWeb: 'string'
-      IdentityPoolId: 'string'
-      IdentityPoolName: 'string'
-      UserPoolArn: 'string'
-      UserPoolId: 'string'
-      UserPoolName: 'string'
+  },
+  "auth": {
+    "maxlascombe": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
-  }
-  function: {
-    APIgetTask: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
+  },
+  "function": {
+    "APIgetTask": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APIprogressToday": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasks": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksDelete": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksDone": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksHistory": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksNew": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksSnooze": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksTop": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "APItasksUpdate": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
-    APIprogressToday: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasks: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksDelete: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksDone: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksHistory: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksNew: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksSnooze: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksTop: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-    APItasksUpdate: {
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      LambdaExecutionRoleArn: 'string'
-      Name: 'string'
-      Region: 'string'
-    }
-  }
-  storage: {
-    history: {
-      Arn: 'string'
-      Name: 'string'
-      PartitionKeyName: 'string'
-      PartitionKeyType: 'string'
-      Region: 'string'
-      StreamArn: 'string'
-    }
-    tasks: {
-      Arn: 'string'
-      Name: 'string'
-      PartitionKeyName: 'string'
-      PartitionKeyType: 'string'
-      Region: 'string'
-      StreamArn: 'string'
-    }
-    tasksv2: {
-      Arn: 'string'
-      Name: 'string'
-      PartitionKeyName: 'string'
-      PartitionKeyType: 'string'
-      Region: 'string'
-      StreamArn: 'string'
+  },
+  "storage": {
+    "history": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    },
+    "tasks": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    },
+    "tasksv2": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
     }
   }
 }
